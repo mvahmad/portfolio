9227b2df43e8833d4334bb56c4dfea8d25da17c2
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-22 18:11:26 +0330

    feat : about me text

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -5,8 +5,13 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`im Ahmad Movahedi aspiring web developer with +2 years exprience in online brand promotion.
-Currently, I focus on develop web page with HTML/CSS and JavaScript in and Next js .`}
+        {`I’m Ahmad Movahedi, a web developer with over 2 years of experience in online brand promotion and digital presence optimization. My background in branding has given me a strong understanding of how design, strategy, and technology work together to create meaningful user experiences.
+
+Currently, I specialize in developing modern, responsive, and high-performance web applications using HTML, CSS, JavaScript, and Next.js. I enjoy translating ideas into clean, efficient, and visually engaging web interfaces that help businesses strengthen their online identity.
+
+I’m passionate about writing maintainable code, optimizing performance, and continuously improving my technical skills within the React ecosystem.
+
+My goal is to collaborate on impactful projects that combine creativity with functionality — building digital products that not only look great but deliver real results.`}
       </p>
       <div className="my-8">
       </div>
