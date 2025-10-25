6061b5774f4ada45cd59b8722ec5d1b060a4300d
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-25 18:08:06 +0330

    fix : home style

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -5,11 +5,11 @@ import Resume from "./components/resume";
 
 export default function Page() {
   return (
-    <>
+    <main className="max-w-6xl mx-auto px-6 py-12">
       <About id={"about"} />
       <Projects id={"projects"} />
       <Feedbacks  id={"feedbacks"} />
       <Resume id={"resume"} />
-    </>
+    </main>
   )
 }
