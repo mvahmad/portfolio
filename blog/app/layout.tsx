a01c1b712965f9b7fd0caab59465ce6633046237
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-25 20:32:29 +0330

    fix : build error

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -2,7 +2,8 @@ import './global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
+import dynamic from "next/dynamic";
+const Navbar = dynamic(() => import("./components/nav").then(mod => mod.Navbar), { ssr: false });
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
