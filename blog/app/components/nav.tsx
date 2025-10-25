f94d367daa3601416993352d3f9a0df53c4ba021
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-26 00:01:34 +0330

    fix : biuld error

--- blog/app/components/nav.tsx
+++ blog/app/components/nav.tsx
@@ -1,15 +1,17 @@
 "use client";
-import Link from 'next/link'
+
+import Link from "next/link";
 import { useEffect, useState } from "react";
+
 const navItems = {
-  "about": { name: "about" },
-  "resume": { name: "resume" },
-  "projects": { name: "projects" },
-  "feedbacks":{name:"feedbacks"}
+  about: { name: "about" },
+  resume: { name: "resume" },
+  projects: { name: "projects" },
+  feedbacks: { name: "feedbacks" },
 };
 
 export function Navbar() {
-  const [activeSection, setActiveSection] = useState("about-section");
+  const [activeSection, setActiveSection] = useState("about");
 
   useEffect(() => {
     const sections = document.querySelectorAll("section");
@@ -22,7 +24,7 @@ export function Navbar() {
           }
         });
       },
-      { threshold: 0.6 } // 60% visible = considered active
+      { threshold: 0.6 }
     );
 
     sections.forEach((section) => observer.observe(section));
@@ -30,32 +32,28 @@ export function Navbar() {
     return () => observer.disconnect();
   }, []);
 
-
-
   return (
     <header className="sticky top-0 z-40 bg-white/60 backdrop-blur-md border-b border-slate-100">
       <div className="lg:sticky lg:top-20">
-        <nav
-         className="max-w-6xl mx-auto px-6 py-4 flex sm:flex-row flex-col items-center sm:justify-between justify-center"
-          id="nav"
-        >
-          <Link href={"/"}  className="text-2xl font-extrabold text-sky-600">Ahmad Movahedi</Link>
-          <div className="flex flex-row md:flex items-center gap-6 text-sm text-slate-600">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                   href={`#${path}`}
-                   className={`hover:text-sky-600 ${activeSection === path ? "text-sky-600 font-semibold" : ""}`}
-                >
-                  {name}
-                </Link>
-              )
-            })}
+        <nav className="max-w-6xl mx-auto px-6 py-4 flex sm:flex-row flex-col items-center sm:justify-between justify-center">
+          <Link href="/" className="text-2xl font-extrabold text-sky-600">
+            Ahmad Movahedi
+          </Link>
+          <div className="flex flex-row items-center gap-6 text-sm text-slate-600">
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={`#${path}`}
+                className={`hover:text-sky-600 ${
+                  activeSection === path ? "text-sky-600 font-semibold" : ""
+                }`}
+              >
+                {name}
+              </Link>
+            ))}
           </div>
         </nav>
       </div>
-      </header>
-
-  )
+    </header>
+  );
 }
